3ed210ba62a864ec8ecd3d765d3337ce4c8ebfee
hinst <hinstance@yandex.ru>
2018-10-24 20:07:43 +0200

    set link to home page

--- _includes/slateHome.js
+++ _includes/slateHome.js
@@ -5,6 +5,6 @@ console.log("Executing slateHome");
         wrapper.appendChild(el);
     }
     var link = document.createElement("a");
-    link.setAttribute("href", "");
+    link.setAttribute("href", "{{ site.url }}{{ site.baseurl }}{{ page.url }}");
     wrap(document.getElementById("project_title"), link);
 })();
