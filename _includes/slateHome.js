fed494bf8978dd34dd8a7c7a3a14b0cbba37b2e3
hinst <hinstance@yandex.ru>
2018-10-24 20:12:53 +0200

    script

--- _includes/slateHome.js
+++ _includes/slateHome.js
@@ -5,6 +5,6 @@ console.log("Executing slateHome");
         wrapper.appendChild(el);
     }
     var link = document.createElement("a");
-    link.setAttribute("href", "{{ site.url }}{{ site.baseurl }}{{ page.url }}");
+    link.setAttribute("href", "{{ site.url }}{{ site.baseurl }}");
     wrap(document.getElementById("project_title"), link);
 })();
